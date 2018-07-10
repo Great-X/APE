23ff42b702dd3f89a606a55e231b18b361333da0
grx <18362916392@163.com>
2018-07-10 12:42:59 +0800

    update progress.

---
+++ reports/report-com.mschlauch.comfortreader/com.mschlauch.comfortreader-1963/sataTimeline.vis.js
@@ -0,0 +1,17 @@
+var groups = new vis.DataSet();
+groups.add({id: 0, content: "SATA Action"});
+var items = new vis.DataSet([
+{start: new Date(1531149861278), title: "Mon Jul 09 11:24:21 EDT 2018@1953", group: 0, screenURL: "step-1953.png", id: 1952, content: "BACK", rect: null},
+{start: new Date(1531149863651), title: "Mon Jul 09 11:24:23 EDT 2018@1954", group: 0, screenURL: "step-1954.png", id: 1953, content: "CLICK", rect: [0,72,411,225]},
+{start: new Date(1531149867616), title: "Mon Jul 09 11:24:27 EDT 2018@1955", group: 0, screenURL: "step-1955.png", id: 1954, content: "CLICK", rect: [0,625,1080,843]},
+{start: new Date(1531149868284), title: "Mon Jul 09 11:24:28 EDT 2018@1956", group: 0, screenURL: "step-1956.png", id: 1955, content: "CLICK", rect: [892,693,1032,774]},
+{start: new Date(1531149868956), title: "Mon Jul 09 11:24:28 EDT 2018@1957", group: 0, screenURL: "step-1957.png", id: 1956, content: "CLICK", rect: [217,288,579,353]},
+{start: new Date(1531149869795), title: "Mon Jul 09 11:24:29 EDT 2018@1958", group: 0, screenURL: "step-1958.png", id: 1957, content: "SCROLL_TOP_DOWN", rect: [108,312,972,1536]},
+{start: new Date(1531149870675), title: "Mon Jul 09 11:24:30 EDT 2018@1959", group: 0, screenURL: "step-1959.png", id: 1958, content: "CLICK", rect: [108,888,972,1080]},
+{start: new Date(1531149872722), title: "Mon Jul 09 11:24:32 EDT 2018@1960", group: 0, screenURL: "step-1960.png", id: 1959, content: "CLICK", rect: [372,1560,636,1704]},
+{start: new Date(1531149873390), title: "Mon Jul 09 11:24:33 EDT 2018@1961", group: 0, screenURL: "step-1961.png", id: 1960, content: "CLICK", rect: [217,288,579,353]},
+{start: new Date(1531149874007), title: "Mon Jul 09 11:24:34 EDT 2018@1962", group: 0, screenURL: "step-1962.png", id: 1961, content: "CLICK", rect: [108,888,972,1080]},
+{start: new Date(1531149874626), title: "Mon Jul 09 11:24:34 EDT 2018@1963", group: 0, screenURL: "step-1963.png", id: 1962, content: "CLICK", rect: [684,1560,948,1704]},
+]);
+var startTime = new Date(1531149861278);
+var endTime = new Date(1531149874626);
---
+++ reports/report-com.mschlauch.comfortreader/index.md
@@ -0,0 +1,8 @@
+title: com.mschlauch.comfortreader
+# com.mschlauch.comfortreader
+
+
+[Google Play Store](https://play.google.com/store/apps/details?id=com.mschlauch.comfortreader)
+
+
+* [com.mschlauch.comfortreader-1963](./com.mschlauch.comfortreader-1963/)
