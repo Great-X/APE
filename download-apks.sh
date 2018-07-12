44da53ccb9c7ee3cd18a461cd6b821f635c45e81
grx <18362916392@163.com>
2018-07-12 16:06:59 +0800

    modify.

--- download-apks.sh
+++ download-apks.sh
@@ -5,6 +5,6 @@ DIR=$(pushd $(dirname $BASH_SOURCE{0}) > /dev/null && pwd  && popd > /dev/null)
 # download apk
 for file in $(ls ${DIR}/urls)
 do
-    wget -P ${DIR}/apks $(cat ${DIR}/urls/${file}) &
+    wget -P ${DIR}/apks $(cat ${DIR}/urls/${file})
 done
 wait
