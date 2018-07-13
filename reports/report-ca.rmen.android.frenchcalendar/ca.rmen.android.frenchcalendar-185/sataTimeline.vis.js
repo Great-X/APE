b80426facd13293fd36810a0b82ab09fc3b827a7
grx <18362916392@163.com>
2018-07-13 18:39:53 +0800

    update progress.

---
+++ reports/report-ca.rmen.android.frenchcalendar/ca.rmen.android.frenchcalendar-185/sataTimeline.vis.js
@@ -0,0 +1,17 @@
+var groups = new vis.DataSet();
+groups.add({id: 0, content: "SATA Action"});
+var items = new vis.DataSet([
+{start: new Date(1531470745987), title: "Fri Jul 13 04:32:25 EDT 2018@175", group: 0, screenURL: "step-175.png", id: 174, content: "CLICK", rect: [756,269,948,413]},
+{start: new Date(1531470746936), title: "Fri Jul 13 04:32:26 EDT 2018@176", group: 0, screenURL: "step-176.png", id: 175, content: "SCROLL_BOTTOM_UP", rect: [756,71,948,611]},
+{start: new Date(1531470748311), title: "Fri Jul 13 04:32:28 EDT 2018@177", group: 0, screenURL: "step-177.png", id: 176, content: "CLICK", rect: [837,84,1080,228]},
+{start: new Date(1531470749110), title: "Fri Jul 13 04:32:29 EDT 2018@178", group: 0, screenURL: "step-178.png", id: 177, content: "SCROLL_TOP_DOWN", rect: [0,240,1080,1776]},
+{start: new Date(1531470750110), title: "Fri Jul 13 04:32:30 EDT 2018@179", group: 0, screenURL: "step-179.png", id: 178, content: "CLICK", rect: [0,72,168,240]},
+{start: new Date(1531470751438), title: "Fri Jul 13 04:32:31 EDT 2018@180", group: 0, screenURL: "step-180.png", id: 179, content: "CLICK", rect: [264,1354,456,1498]},
+{start: new Date(1531470752442), title: "Fri Jul 13 04:32:32 EDT 2018@181", group: 0, screenURL: "step-181.png", id: 180, content: "SCROLL_RIGHT_LEFT", rect: [768,812,960,1352]},
+{start: new Date(1531470753829), title: "Fri Jul 13 04:32:33 EDT 2018@182", group: 0, screenURL: "step-182.png", id: 181, content: "CLICK", rect: [516,774,708,972]},
+{start: new Date(1531470754698), title: "Fri Jul 13 04:32:34 EDT 2018@183", group: 0, screenURL: "step-183.png", id: 182, content: "SCROLL_TOP_DOWN", rect: [264,1173,456,1680]},
+{start: new Date(1531470756109), title: "Fri Jul 13 04:32:36 EDT 2018@184", group: 0, screenURL: "step-184.png", id: 183, content: "LONG_CLICK", rect: [516,630,708,774]},
+{start: new Date(1531470759170), title: "Fri Jul 13 04:32:39 EDT 2018@185", group: 0, screenURL: "step-185.png", id: 184, content: "CLICK", rect: [528,1354,720,1498]},
+]);
+var startTime = new Date(1531470745987);
+var endTime = new Date(1531470759170);
---
+++ reports/report-ca.rmen.android.frenchcalendar/index.md
@@ -0,0 +1,8 @@
+title: ca.rmen.android.frenchcalendar
+# ca.rmen.android.frenchcalendar
+
+
+[Google Play Store](https://play.google.com/store/apps/details?id=ca.rmen.android.frenchcalendar)
+
+
+* [ca.rmen.android.frenchcalendar-185](./ca.rmen.android.frenchcalendar-185/)
